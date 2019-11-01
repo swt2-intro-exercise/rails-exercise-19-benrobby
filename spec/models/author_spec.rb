b3d0f1059907cd99d1a95bdb90c9f0a2e949841f
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2019-11-01 12:58:12 +0100

    use before hook

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,17 +1,19 @@
+require 'rails_helper'
 
 describe Author, type: :model do
 
-  author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
-
+  before(:each) do
+    @author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+  end
 
   it 'Should create and store new author' do
-    expect(author.first_name).to eq('Alan')
-    expect(author.last_name).to eq('Turing')
-    expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+    expect(@author.first_name).to eq('Alan')
+    expect(@author.last_name).to eq('Turing')
+    expect(@author.homepage).to eq('http://wikipedia.org/Alan_Turing')
   end
 
   it 'should have the full name' do
-    expect(author.name).to eq('Alan Turing')
+    expect(@author.name).to eq('Alan Turing')
   end
 
 end
