60b071d309bdb96462424ae9d857ae8aa021094e
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2019-11-01 12:59:53 +0100

    issue #5 testcase

---
+++ spec/features/author/show_author_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Show author page', type: :feature do
+  
+  before(:each) do
+    @alan = FactoryBot.create :author
+  end
+
+  it 'should visit the page' do
+    visit author_path(@alan)
+    expect(page).to have_text('Alan')
+    expect(page).to have_text('Turing')
+    expect(page).to have_text('http://wikipedia.de/Alan_Turing')
+  end
+end
